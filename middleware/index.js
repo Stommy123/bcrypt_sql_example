f56a71ea2b3958f03e2d930dd0a80b5fa5f55db4
tommy <tommy.suwunrut@gmail.com>
2020-06-02 12:07:51 -0400

    removed coercion

--- middleware/index.js
+++ middleware/index.js
@@ -4,8 +4,8 @@ export const withAuth = async (req, res, next) => {
   try {
     const { userId } = req.cookies;
 
-    const user = await DB.single('SELECT * FROM users WHERE id = $1 LIMIT 1', [~~userId]);
-
+    const user = await DB.single('SELECT * FROM users WHERE id = $1 LIMIT 1', [userId]);
+    console.log('user?', user);
     if (!user) throw new Error('UNAUTHENTICATED');
 
     next();
